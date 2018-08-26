41d6fbf7b3326597b0f8a50a4bb405046593ad43
Tim Schlueter <schlueter.tim@linux.com>
2018-08-26 13:50:50 -0700

    Quite a bit of formatting and other minor changes

--- cshatag.c
+++ cshatag.c
@@ -15,18 +15,21 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <errno.h>
+#include <stdarg.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/xattr.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <openssl/sha.h>
-#include <time.h>
 #include <string.h>
-#include <errno.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/xattr.h>
+#include <time.h>
 
-#define BUFSZ 8192
-#define HASHLEN 256/8
+#include <openssl/sha.h>
+
+#define BUFSZ 65536
+#define HASHLEN SHA256_DIGEST_LENGTH
 
 /**
  * Holds a file's metadata
@@ -35,131 +38,150 @@ typedef struct
 {
 	unsigned long long s;
 	unsigned long ns;
-	char sha256[HASHLEN*2+1];
+	char sha256[HASHLEN * 2 + 1];
 } xa_t;
+
+static void die(const char *fmt, ...) __attribute__((noreturn, format(printf, 1, 2)));
+
+static void die(const char *fmt, ...)
+{
+	va_list ap;
+
+	va_start(ap, fmt);
+	vfprintf(stderr, fmt, ap);
+	va_end(ap);
+
+	exit(EXIT_FAILURE);
+}
 
 /**
  * ASCII hex representation of char array
  */
-char * bin2hex(unsigned char * bin, long len, char * out)
+void bin2hex(unsigned char *bin, char *out, size_t len)
 {
 	char hexval[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
-	int j;
-	for(j=0;j<len;j++)
-	{
-		out[2*j] = hexval[((bin[j] >> 4) & 0x0F)];
-		out[2*j+1] = hexval[(bin[j]) & 0x0F];
+	size_t i;
+
+	for (i = 0; i < len; i++) {
+		out[2 * i] = hexval[((bin[i] >> 4) & 0x0F)];
+		out[2 * i + 1] = hexval[(bin[i]) & 0x0F];
 	}
-	out[2*len]=0;
-	return out;
+
+	out[2 * len] = 0;
 }
 
 /**
  * sha256 of contents of f, ASCII hex representation
  */
-char * fhash(FILE *f, char *hex)
+void fhash(FILE *f, char *hex)
 {
 	SHA256_CTX c;
-	char buf[BUFSZ];
+	char *buf;
+	unsigned char *hash;
 	size_t len;
+
+	buf = malloc(BUFSZ);
+	hash = calloc(1, HASHLEN);
+
+	if (NULL == buf || NULL == hash)
+		die("Insufficient memory for hashing file");
 
 	SHA256_Init(&c);
 
-	while( (len=fread(buf, 1, BUFSZ,f) ) )
-	{
+	while ((len = fread(buf, 1, BUFSZ, f)) != 0)
 		SHA256_Update(&c, buf, len);
-	}
 
-	unsigned char * hash;
-	hash=calloc(1,HASHLEN);
-	if(NULL == hash)
-	{
-		fprintf(stderr, "Insufficient memory for hashing file");
-		exit(EXIT_FAILURE);
-	}
 	SHA256_Final(hash, &c);
 
-	//printf("%s\n",bin2hex(hash,HASHLEN));
+	bin2hex(hash, hex, HASHLEN);
 
-	return bin2hex(hash,HASHLEN,hex);
+	free(buf);
+	free(hash);
 }
 
 /**
  * Nanosecond precision mtime of a file
  */
-xa_t getmtime(FILE *f)
+void getmtime(FILE *f, xa_t *actual)
 {
-	xa_t actual;
-
-	int fd=fileno(f);
+	int fd = fileno(f);
 	struct stat buf;
-	fstat(fd, &buf);
-	if(!S_ISREG(buf.st_mode))
-	{
-		fprintf(stderr,"Error: this is not a regular file\n");
-		exit(3);
-	}
-	actual.s=buf.st_mtim.tv_sec;
-	actual.ns=buf.st_mtim.tv_nsec;
 
-	return actual;
+	fstat(fd, &buf);
+
+	if (!S_ISREG(buf.st_mode))
+		die("Error: this is not a regular file\n");
+
+	actual->s = buf.st_mtim.tv_sec;
+	actual->ns = buf.st_mtim.tv_nsec;
 }
 
 /**
  * File's actual metadata
  */
-xa_t getactualxa(FILE *f)
+void getactualxa(FILE *f, xa_t *actual)
 {
-	xa_t actual;
 	/*
 	 * Must read mtime *before* file hash,
 	 * if the file is being modified, hash will be invalid
 	 * but timestamp will be outdated anyway
 	 */
-	actual=getmtime(f);
+	getmtime(f, actual);
 
 	/*
 	 * Compute hash
 	 */
-	fhash(f,actual.sha256);
-
-	return actual;
+	fhash(f, actual->sha256);
 }
 
 /**
  * File's stored metadata
  */
-xa_t getstoredxa(FILE *f)
+void getstoredxa(FILE *f, xa_t *stored)
 {
-	int fd=fileno(f);
-	xa_t xa;
-	xa.s=0;
-	xa.ns=0;
+	int err;
+	int fd = fileno(f);
+	/* Example: 1335974989.123456789 => len=20 */
+	char ts[32];
+	ssize_t len;
 
 	/*
 	 * Initialize to zero-length string - if fgetxattr fails this is what we get
 	 */
-	xa.sha256[0]=0;
-	fgetxattr(fd, "user.shatag.sha256", xa.sha256, sizeof(xa.sha256));
-	xa.sha256[HASHLEN*2]=0;
+	len = fgetxattr(fd, "user.shatag.sha256", stored->sha256, sizeof(stored->sha256));
+	stored->sha256[len] = '\0';
 
-	/*
-	 * Example:
-	 * 1335974989.123456789
-	 *    10     .     9     => len=20
-	 */
-	char ts[100];
+	if (len < 0) {
+		if (errno == ENODATA)
+			memset(stored->sha256, 0, sizeof(stored->sha256)); /* Ignore if no attr exists. */
+		else
+			die("Error retrieving stored attributes: %m\n");
+	}
+
 	/*
 	 * Initialize to zero-length string - if fgetxattr fails this is what we get
 	 */
-	ts[0]=0;
-	fgetxattr(fd, "user.shatag.ts", ts, sizeof(ts));
+	len = fgetxattr(fd, "user.shatag.ts", ts, sizeof(ts));
+	ts[len] = '\0';
+	if (len < 0) {
+		if (errno == ENODATA)
+			memset(stored, 0, sizeof(*stored)); /* Ignore if no attr exists. */
+		else
+			die("Error retrieving stored attributes: %m\n");
+
+		return;
+	}
+
 	/*
 	 * If sscanf fails (because ts is zero-length) variables stay zero
 	 */
-	sscanf(ts,"%llu.%lu",&xa.s,&xa.ns);
-
-	return xa;
+	err = sscanf(ts, "%llu.%lu", &stored->s, &stored->ns);
+	if (err == 1)
+		stored->ns = 0;
+	else if (err != 2)
+		die("Failed to read timestamp: %m\n");
+	else if (stored->ns >= 1000000000)
+		die("Invalid timestamp (ns too large): %s\n", ts);
 }
 
 /**
@@ -167,114 +189,104 @@ xa_t getstoredxa(FILE *f)
  */
 int writexa(FILE *f, xa_t xa)
 {
-	int fd=fileno(f);
-	int flags=0,err=0;
+	int fd = fileno(f);
+	int flags = 0;
+	int err = 0;
+	char buf[32];
 
-	char buf [100];
-	snprintf(buf,sizeof(buf),"%llu.%09lu",xa.s,xa.ns);
-	err=fsetxattr(fd, "user.shatag.ts", buf, strlen(buf), flags);
-	err|=fsetxattr(fd, "user.shatag.sha256", &xa.sha256, sizeof(xa.sha256), flags);
+	err = fsetxattr(fd, "user.shatag.sha256", &xa.sha256, sizeof(xa.sha256), flags);
+	if (err != 0)
+		return err;
 
+	snprintf(buf, sizeof(buf), "%llu.%09lu", xa.s, xa.ns);
+	err = fsetxattr(fd, "user.shatag.ts", buf, strlen(buf), flags);
 	return err;
 }
 
 /**
  * Pretty-print metadata
  */
-char * formatxa(xa_t s)
+char *formatxa(xa_t s)
 {
-	char * buf;
-	char * prettysha;
-	int buflen=HASHLEN*2+100;
-	buf=calloc(1,buflen);
+	char *buf;
+	char *prettysha;
+	int buflen = HASHLEN * 2 + 32;
 
-	if(NULL == buf)
-	{
-		fprintf(stderr, "Insufficient space to store hash stringed");
-		exit(EXIT_FAILURE);
-	}
+	buf = calloc(1, buflen);
+	if (NULL == buf)
+		die("Insufficient space to store hash stringed");
 
-	if(strlen(s.sha256)>0)
-		prettysha=s.sha256;
+	if (strlen(s.sha256) > 0)
+		prettysha = s.sha256;
 	else
-		prettysha="0000000000000000000000000000000000000000000000000000000000000000";
-	snprintf(buf,buflen,"%s %010llu.%09lu", prettysha, s.s, s.ns);
+		prettysha = "0000000000000000000000000000000000000000000000000000000000000000";
+
+	snprintf(buf, buflen, "%s %llu.%09lu", prettysha, s.s, s.ns);
+
 	return buf;
 }
 
-int main( int argc, const char* argv[] )
+int main(int argc, char *argv[])
 {
-	char const * myname;
-	myname=argv[0];
+	char const *myname = argv[0];
 
-	if(argc != 2)
-	{
-		fprintf(stderr,"Usage: %s FILE\n",myname);
-		exit(1);
-	}
+	if (argc != 2)
+		die("Usage: %s FILE\n", myname);
 
-	char const * fn;
-	fn=argv[1];
+	char const *fn = argv[1];
 
-	FILE *f;
-	f=fopen(fn,"r");
-	if(!f)
-	{
-		fprintf(stderr,"Error: could not open file \"%s\": %s\n",fn,strerror(errno));
-		exit(2);
-	}
+	FILE *f = fopen(fn, "r");
+	if (!f)
+		die("Error: could not open file \"%s\": %m\n", fn);
 
 	xa_t s;
-	s=getstoredxa(f);
 	xa_t a;
-	a=getactualxa(f);
-	int needsupdate=0;
-	int havecorrupt=0;
+	int needsupdate = 0;
+	int havecorrupt = 0;
 
-	if(s.s==a.s && s.ns==a.ns)
-	{
+	getstoredxa(f, &s);
+	getactualxa(f, &a);
+
+	if (s.s == a.s && s.ns == a.ns) {
 		/*
 		 * Times are the same, go ahead and compare the hash
 		 */
-		if(strcmp(s.sha256,a.sha256)!=0)
-		{
+		if (strcmp(s.sha256, a.sha256) != 0) {
 			/*
 			 * Hashes are different, but this may be because
 			 * the file has been modified while we were computing the hash.
 			 * So check if the mtime ist still the same.
 			 */
 			xa_t a2;
-			a2=getmtime(f);
-			if(s.s==a2.s && s.ns==a2.ns)
-			{
+			getmtime(f, &a2);
+
+			if (s.s == a2.s && s.ns == a2.ns) {
 				/*
 				 * Now, this is either data corruption or somebody modified the file
 				 * and reset the mtime to the last value (to hide the modification?)
 				 */
-				fprintf(stderr,"Error: corrupt file \"%s\"\n",fn);
-				printf("<corrupt> %s\n",fn);
-				printf(" stored: %s\n actual: %s\n",formatxa(s),formatxa(a));
-				needsupdate=1;
-				havecorrupt=1;
+				fprintf(stderr, "Error: corrupt file \"%s\"\n", fn);
+				printf("<corrupt> %s\n", fn);
+				printf(" stored: %s\n", formatxa(s));
+				printf(" actual: %s\n", formatxa(a));
+				needsupdate = 1;
+				havecorrupt = 1;
 			}
 		}
 		else
-			printf("<ok> %s\n",fn);
+			printf("<ok> %s\n", fn);
 	}
-	else
-	{
-		printf("<outdated> %s\n",fn);
-		printf(" stored: %s\n actual: %s\n",formatxa(s),formatxa(a));
-		needsupdate=1;
-	}
-
-	if(needsupdate && writexa(f,a)!=0)
-	{
-		fprintf(stderr,"Error: could not write extended attributes to file \"%s\": %s\n",fn,strerror(errno));
-		exit(4);
+	else {
+		printf("<outdated> %s\n", fn);
+		printf(" stored: %s\n", formatxa(s));
+		printf(" actual: %s\n", formatxa(a));
+		needsupdate = 1;
 	}
 
-	if(havecorrupt)
+	if (needsupdate && writexa(f,a) != 0)
+		die("Error: could not write extended attributes to file \"%s\": %m\n", fn);
+
+	if (havecorrupt)
 		return 5;
 	else
 		return 0;
